957f56e37551520481ac5a9933939e92a29e2542
hishabee71 <maruf@hishabee.io>
2024-02-17 12:01:32 +0600

    style: menu icons

--- src/components/layouts/dashboard/AsideBar.tsx
+++ src/components/layouts/dashboard/AsideBar.tsx
@@ -16,7 +16,7 @@ const AsideBar = ({ menuOpen, setMenuOpen }: IMenuOpenProps) => {
     return (
         <aside className={`bg-white dark:bg-primary-90 h-screen fixed ${menuOpen ? 'w-[30rem] xl:w-[34.4rem] left-0' : '-left-[30rem] xl:left-0 w-[30rem] xl:w-[8rem]'} duration-500 z-50`}>
             <div className={`h-[7.2rem] border-b border-primary-10 dark:border-primary-80 px-space16 flex items-center justify-between sticky top-0`}>
-                <Link href="/dashboard">
+                <Link href="/home">
                     <Image
                         alt="logo"
                         height={32}
@@ -40,7 +40,7 @@ const AsideBar = ({ menuOpen, setMenuOpen }: IMenuOpenProps) => {
                                 onClick={() => setActive(subMenu ? active : active === menu.id ? null : menu.id)}
                                 className={`flex items-center p-space8 w-full hover:bg-primary-10 dark:hover:bg-[#171717] duration-300 rounded-md relative after:absolute after:w-[.4rem] hover:after:h-full after:duration-300 after:rounded-[1rem] after:-left-3 after:top-1/2 after:transform after:-translate-y-1/2 after:bg-primary-100 dark:after:bg-primary-60 ${pathname.includes(menu.link) ? 'after:h-full bg-primary-10 dark:bg-[#171717]' : 'after:h-0'}`}>
 
-                                <div className="h-[3.4rem] w-[3.6rem] relative z-20 bg-primary-50">img</div>
+                                <Image src={menu.image} alt="" height={24} width={24} />
                                 <span className={`block text-text500 font-medium dark:text-text300 transition-all duration-700 ${menuOpen ? 'ml-space10' : 'w-0 h-0 overflow-hidden'}`}>{menu.title}</span>
 
                                 {menu.children && (
