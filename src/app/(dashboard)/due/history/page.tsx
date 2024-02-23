0bffd6079cdb5190821b58cf41543a4b6c43a841
hishabee71 <maruf@hishabee.io>
2024-02-23 23:30:51 +0600

    style: due history page design done

--- src/app/(dashboard)/due/history/page.tsx
+++ src/app/(dashboard)/due/history/page.tsx
@@ -1,20 +1,15 @@
 import React from 'react'
-import SellDrawers from '@/components/sell/drawers'
-import SellDialogs from '@/components/sell/dialogs'
-import HistoryTable from '@/components/sell/history/HistoryTable'
-import HistoryHeader from '@/components/sell/history/HistoryHeader'
+import HistoryTable from '@/components/due/HistoryTable'
+import HistoryHeader from '@/components/due/HistoryHeader'
+import HistoryReport from '@/components/due/HistoryReport'
 
 const SellHistory = () => {
     return (
-        <>
-            <div className='space-y-space16 h-full w-full'>
-                <HistoryHeader />
-                <HistoryTable />
-            </div>
-
-            <SellDrawers />
-            <SellDialogs />
-        </>
+        <div className='space-y-space16 h-full w-full'>
+            <HistoryHeader />
+            <HistoryReport />
+            <HistoryTable />
+        </div>
     )
 }
 
