23a17bbac74a87694c71506b6a5756da982759e8
hishabee71 <maruf@hishabee.io>
2024-03-06 16:21:18 +0600

    style: added dark mode style in online-shop

--- src/components/online-shop/orders/orderTypeWiseStyled.ts
+++ src/components/online-shop/orders/orderTypeWiseStyled.ts
@@ -1,42 +1,45 @@
 export const orderTypeWiseStyled = (type: string) => {
 
     const textVariant = (): "blue" | "warning" | "success" | "error" | undefined => {
-        if (type === 'new') {
-            return 'blue'
-        } else if (type === 'pending') {
-            return 'warning'
-        } else if (type === 'complete') {
-            return 'success'
-        } else if (type === 'cancelled') {
-            return 'error'
-        } else {
-            return undefined
+        switch (type) {
+            case 'new':
+                return 'blue'
+            case 'pending':
+                return 'warning'
+            case 'complete':
+                return 'success'
+            case 'cancelled':
+                return 'error'
+            default:
+                return undefined
         }
     }
     const textBackground = () => {
-        if (type === 'new') {
-            return 'bg-blue-100'
-        } else if (type === 'pending') {
-            return 'bg-warning-10'
-        } else if (type === 'complete') {
-            return 'bg-success-20'
-        } else if (type === 'cancelled') {
-            return 'bg-error-10'
-        } else {
-            return undefined
+        switch (type) {
+            case 'new':
+                return 'bg-blue-100 dark:bg-primary-80'
+            case 'pending':
+                return 'bg-warning-10 dark:bg-primary-80'
+            case 'complete':
+                return 'bg-success-20 dark:bg-primary-80'
+            case 'cancelled':
+                return 'bg-error-10 dark:bg-primary-80'
+            default:
+                return undefined
         }
     }
     const title = () => {
-        if (type === 'new') {
-            return 'New Order'
-        } else if (type === 'pending') {
-            return 'Pending'
-        } else if (type === 'complete') {
-            return 'Complete'
-        } else if (type === 'cancelled') {
-            return 'Cancelled'
-        } else {
-            return undefined
+        switch (type) {
+            case 'new':
+                return 'New Order'
+            case 'pending':
+                return 'Pending'
+            case 'complete':
+                return 'Complete'
+            case 'cancelled':
+                return 'Cancelled'
+            default:
+                return undefined
         }
     }
 
