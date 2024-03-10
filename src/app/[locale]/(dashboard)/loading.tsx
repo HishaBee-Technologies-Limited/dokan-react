d1fac17f5b7de5a52c758ca5b4de2fccf108f1f5
hishabee71 <maruf@hishabee.io>
2024-03-10 11:29:48 +0600

    style: dashboard loading screen design

---
+++ src/app/[locale]/(dashboard)/loading.tsx
@@ -0,0 +1,8 @@
+import CommonLoading from "@/components/layouts/CommonLoading";
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <CommonLoading />
+  );
+}
