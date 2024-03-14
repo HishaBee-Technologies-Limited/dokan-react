45643d1e91d4967b148fa71a000382fa19063d73
Nazu <nazu@hishabee.io>
2024-03-14 14:52:23 +0600

    feat: working on online shop order page

--- src/components/online-shop/orders/FilteringTabs.tsx
+++ src/components/online-shop/orders/FilteringTabs.tsx
@@ -1,7 +1,8 @@
 'use client'
 import React from 'react'
-import CustomTab from '@/components/common/Tab'
+import CustomTab, { ITabOption } from "@/components/common/Tab";
 import { useOnlineShopStore } from '@/stores/useOnlineShopStore'
+import { useRouter, useSearchParams } from "next/navigation";
 
 const filteringOption = [
     {
@@ -27,11 +28,21 @@ const filteringOption = [
 ]
 
 const FilteringTabs = () => {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+
     const orderFilterTab = useOnlineShopStore(state => state.orderFilterTab)
     const setOrderFilterTab = useOnlineShopStore(state => state.setOrderFilterTab)
 
+    const handleTab = (tab: ITabOption) => {
+        const currentParams = new URLSearchParams(searchParams.toString());
+        currentParams.set('activeTab', tab.value);
+        router.replace(`?${currentParams.toString()}`, { scroll: false });
+        setOrderFilterTab(tab.value)
+    }
+
     return (
-        <CustomTab data={filteringOption} active={orderFilterTab} handleChange={(tab) => setOrderFilterTab(tab.value)} />
+        <CustomTab data={filteringOption} active={orderFilterTab} handleChange={handleTab} />
     )
 }
 
