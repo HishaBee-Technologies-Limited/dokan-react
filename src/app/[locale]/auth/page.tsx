8cc401edb2b32d7f8b4b9536ad88a0d1aee6a0af
Nazu <nazu@hishabee.io>
2024-03-18 14:51:37 +0600

    fix: crush issue auth page

--- src/app/[locale]/auth/page.tsx
+++ src/app/[locale]/auth/page.tsx
@@ -1,8 +1,6 @@
 import GiveNumber from "@/components/auth/GiveNumber";
-import { getTranslations } from "next-intl/server";
 
 export default async function Home() {
-  const t = await getTranslations("Index");
   return (
     <main>
       <GiveNumber />
