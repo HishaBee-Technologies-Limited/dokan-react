6db36dcc61c8f1ae48de1fccef4196cf4bf12d8b
hishabee71 <maruf@hishabee.io>
2024-03-21 11:22:12 +0600

    feat: modefied features

--- src/stores/useContactStore.ts
+++ src/stores/useContactStore.ts
@@ -1,38 +1,28 @@
 
-import { create } from 'zustand'
-import { ContactEnum } from '@/enum/contact';
-import { immer } from 'zustand/middleware/immer'
+import { create } from 'zustand';
 import { IUserResponse } from '@/types/contact/partyResponse';
 
 type ContactState = {
-    activeTab: string;
-    activeUserItem: number;
+    party: IUserResponse | undefined;
     contactDrawerState: {
         open: boolean;
         header?: string;
     };
-    party: IUserResponse | undefined;
 }
 
 type ContactActions = {
-    setActiveTab: (tab: string) => void;
-    setActiveUserItem: (index: number) => void;
+    setParty: (party: IUserResponse) => void;
     setContactDrawerState: (params: {
         open: boolean;
         header?: string | undefined;
     }) => void;
-    setParty: (party: IUserResponse) => void;
 }
 
 export const useContactStore = create<ContactState & ContactActions>()((set) => ({
-    activeUserItem: 0,
-    activeTab: ContactEnum.ACTIVE_TAB,
-    contactDrawerState: { open: false, header: undefined },
     party: undefined,
+    contactDrawerState: { open: false, header: undefined },
 
     // Update state-------------------------------------
-    setActiveTab: (tab) => set({ activeTab: tab }),
-    setActiveUserItem: (index) => set({ activeUserItem: index }),
+    setParty: (party) => set({ party }),
     setContactDrawerState: (params) => set({ contactDrawerState: params }),
-    setParty: (party) => set({ party })
 }))
--- src/stores/useDueStore.ts
+++ src/stores/useDueStore.ts
@@ -1,12 +1,7 @@
 
 import { create } from 'zustand'
-import { ContactEnum } from '@/enum/contact';
-import { immer } from 'zustand/middleware/immer'
-import { DueEnum } from '@/enum/due';
 
 type DueState = {
-    activePageTab: string;
-    activeUserItem: number;
     drawerState: {
         open: boolean;
         header?: string;
@@ -18,8 +13,6 @@ type DueState = {
 }
 
 type DueActions = {
-    setActivePageTab: (tab: string) => void;
-    setActiveUserItem: (index: number) => void;
     setDrawerState: (params: {
         open: boolean;
         header?: string | undefined;
@@ -30,15 +23,11 @@ type DueActions = {
     }) => void;
 }
 
-export const useDueStore = create<DueState & DueActions>()(immer((set) => ({
-    activeUserItem: 0,
-    activePageTab: DueEnum.CUSTOMER,
+export const useDueStore = create<DueState & DueActions>()((set) => ({
     drawerState: { open: false, header: undefined },
     dialogState: { open: false, header: undefined },
 
     // Update state-------------------------------------
-    setActivePageTab: (tab) => set({ activePageTab: tab }),
-    setActiveUserItem: (index) => set({ activeUserItem: index }),
     setDrawerState: (params) => set({ drawerState: params }),
     setDialogState: (params) => set({ dialogState: params })
-})))
+}))
