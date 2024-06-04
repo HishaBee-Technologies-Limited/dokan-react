29a5ee13221d9824698a6535095152a6ef3e67a1
shimul <20032077+shimulH@users.noreply.github.com>
2024-06-04 13:09:24 +0600

    feat: adds logout button in the shop screen

--- src/components/layouts/shop/index.tsx
+++ src/components/layouts/shop/index.tsx
@@ -3,13 +3,20 @@ import React from 'react';
 import { CopyRight } from '@/components/layouts/auth/CopyRight';
 import BrandingLogo from '@/components/layouts/auth/BrandingLogo';
 import TranslateToggle from '@/components/layouts/auth/TranslationToggle';
+import { logout } from '@/actions/logout';
 
 const ShopLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-screen">
-      <section className="h-full w-full flex flex-col gap-space24 justify-between p-space16 sm:p-space32 overflow-y-scroll">
-        <div className="flex justify-end w-full">
+      <section className="h-full w-full flex flex-col gap-space24 justify-between  p-space16 sm:p-space32 overflow-y-scroll">
+        <div className="flex items-center gap-2 justify-end w-full">
           <TranslateToggle />
+          <div
+            onClick={() => logout()}
+            className="p-2 px-4 bg-black text-white rounded-sm cursor-pointer"
+          >
+            Logout
+          </div>
         </div>
 
         <div className="max-w-[78rem] w-full mx-auto space-y-space16">
