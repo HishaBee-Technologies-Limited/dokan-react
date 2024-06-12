15d1e6f8dcbdb264fe83acc67247a1b4233bb817
Nazmul Haque <bpinazmul18@gmail.com>
2024-06-12 09:52:27 +0600

    feat: add to the supplier page

--- src/app/[locale]/(dashboard)/business/reports/supplier/page.tsx
+++ src/app/[locale]/(dashboard)/business/reports/supplier/page.tsx
@@ -1,3 +1,17 @@
-export default function Supplier() {
-  return <div>Supplier</div>;
+import PageHeader from '@/components/product-report/PageHeader';
+import SupplierReportTable from '@/components/supplier-report/SupplierReportTable';
+import { getSupplierReport } from '@/actions/supplier-report/getSupplierReport';
+
+export default async function Supplier({ searchParams }: any) {
+  const supplierReport = await getSupplierReport(
+    searchParams.start_date,
+    searchParams.end_date
+  );
+
+  return (
+    <div className="space-y-space16 h-full w-full">
+      <PageHeader />
+      <SupplierReportTable supplierReport={supplierReport?.data as any} />
+    </div>
+  );
 }
