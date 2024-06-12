e53340c0c165cd9b92ca40557f3935577c41bc9c
Nazmul Haque <bpinazmul18@gmail.com>
2024-06-12 09:22:52 +0600

    feat: working on stock report

--- src/app/[locale]/(dashboard)/business/reports/stock/page.tsx
+++ src/app/[locale]/(dashboard)/business/reports/stock/page.tsx
@@ -1,3 +1,17 @@
-export default function Stock() {
-  return <div>Stock</div>;
+import PageHeader from '@/components/stock-report/PageHeader';
+import { getStockReports } from '@/actions/business-reports/getStockReport';
+import StockReportTable from '@/components/stock-report/StockReportTable';
+
+export default async function Stock({ searchParams }: any) {
+  const stockReport = await getStockReports(
+    searchParams.start_date,
+    searchParams.end_date
+  );
+
+  return (
+    <div className="space-y-space16 h-full w-full">
+      <PageHeader />
+      <StockReportTable stockReport={stockReport?.data as any} />
+    </div>
+  );
 }
---
+++ src/types/stockReport.ts
@@ -0,0 +1,7 @@
+export interface IStockReportsResponse {
+  product_name: string;
+  stock_in_count: string;
+  stock_in_amount: number;
+  stock_out_count: number;
+  stock_out_amount: number;
+}
