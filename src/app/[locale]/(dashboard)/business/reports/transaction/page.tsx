2073dd1571d5d432a3c076ddb9c451bb0a33c7d8
Nazmul Haque <bpinazmul18@gmail.com>
2024-06-12 08:40:37 +0600

    feat: working on transaction report header

--- src/app/[locale]/(dashboard)/business/reports/transaction/page.tsx
+++ src/app/[locale]/(dashboard)/business/reports/transaction/page.tsx
@@ -1,3 +1,13 @@
+import React from 'react';
+import PageHeader from '@/components/transaction-reports/PageHeader';
+
 export default function Transaction() {
-  return <div>Transaction</div>;
+  return (
+    <>
+      <div className="space-y-space16 h-full w-full">
+        <PageHeader />
+      </div>
+      {/*Dialog*/}
+    </>
+  );
 }
